d0b50f7e04f4766d3c2bc1eaabf9f1a4b08c3e9c
kamauvick <kamauvick@outlook.com>
2020-02-22 22:45:10 +0300

    Refactor code on the test.py file.

--- test.py
+++ test.py
@@ -5,11 +5,12 @@ def read_data():
     Function to read data from a file line by line while removing the new lines.
     """
     data = []
+    # Read file data
     with open("b_read_on.txt", "r+") as file:
         i = 0
         while True:
             file_data = file.readline()
-            data.append(file_data.replace("\n", ''))
+            data.append(file_data.replace("\n", '')) #Remove newlines
             if file_data == '':
                 break
         i += 1
@@ -21,52 +22,61 @@ def filter_data():
     Remove the first two rows from the data
     """
     file_data = read_data()
+
+    # Loop through the file data and append it to an array
     data = lambda x : [x for x in file_data]
     my_data = data(file_data)
+
+    # Ignore the leading 2 lines of the data
     filtered_data = my_data[2:]
     return filtered_data
 
 
-def get_highest_signup(even_indice_list, n):
+
+def check_highest_second_index(some_list):
     """
-    Split an array into multiple arrays and get the largest value of the arrays[1] value (signup time)
+    A function to get the largest signup days by checking which is the largest value
+    on index[1] of every array element
     """
-    # NB: This function does NOT function as expected, help fix it :)
-    max = even_indice_list[0]
-    for i in range(1, n):
-        if even_indice_list[i] > max:
-            max = even_indice_list[i]
-    return max  
+    # Remove all spaces from the list elements
+    list_with_no_spaces = [i.replace(' ', ',') for i in some_list]
+    print(list_with_no_spaces)
+
+    list_of_lists = []
+    counter = 0
+    for i in list_with_no_spaces:
+        if int(i[2]) > counter:
+            counter = int(i[2])
+    return counter
+        
+
 
 
 
 def get_highest_signup_days():
     """
-    Remove all spaces before, between and after values. Only filter out even indices and push their value 
-    to a new array (even indices represent the libraries).
-    get the highest library signup time and return it's value
+    Remove all spaces before, between and after values. 
+    Filter out even indices and push their value to a new array (even indices represent the libraries).
     """
+
     # NB: This function also has some bugs, works partially
     new_data = []
     data = filter_data()
-    unspaced_data = ([re.sub('\s+', ' ', i).strip() for i in data if i is not ''][:-1])
-    # print(unspaced_data)
-    int_data = [str(i) for i in unspaced_data]
+    unspaced_data = ([re.sub('\s+', ' ', i).strip() for i in data if i is not ''][:-1]) #Remove spaces
+    int_data = [str(i) for i in unspaced_data] #Convert array values to integers
     even_indices = []
     for i in int_data:
-        if int_data.index(i) % 2 == 0:
+        if int_data.index(i) % 2 == 0: #Get even indices
             even_indices.append(i)
-    print(f'The number of libraries is {len(even_indices)}')
-    highest_signup_days = get_highest_signup(even_indices, len(even_indices))
-    highest_signup_days = list(str(highest_signup_days))
-    hsd = ''.join([re.sub( '\s+','', i) for i in highest_signup_days])
-    print(str(hsd)[1])
+    print(even_indices)
+    highest_signup_days = check_highest_second_index(even_indices)
+    
+    print('\n')
+    print(f'The number of libraries is : {len(even_indices)}')
+    print(f'The highest signup days is : {highest_signup_days}')
+ 
 
-    return str(hsd)[1]
-
-
-# Run all the functions
+# Run all the functions 
 if __name__ == "__main__":
     data = filter_data()
-    filter_data()
     get_highest_signup_days()
